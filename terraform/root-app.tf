765e78d26f8e5762b9d0ad398364030046defcfc
Mario Tema <mario.tema@adesso.de>
2026-01-30 13:04:14 +0100

    DX-231 add argocd structure

---
+++ terraform/root-app.tf
@@ -0,0 +1,49 @@
+resource "kubernetes_namespace" "root_apps" {
+  metadata {
+    name = local.namespace
+
+    annotations = {
+      name = local.namespace
+    }
+
+    labels = {
+      namespace = local.namespace
+    }
+  }
+}
+
+resource "kubernetes_manifest" "argo_apps" {
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = local.namespace
+      namespace = local.namespace
+    }
+    spec = {
+      project = local.namespace
+
+      source = {
+        repoURL        = "https://github.com/microtema/argo-cd.git"
+        targetRevision = "main"
+        path           = "argocd/apps"
+      }
+
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = local.namespace
+      }
+
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+        syncOptions = ["CreateNamespace=false"]
+      }
+    }
+  }
+
+  depends_on = [helm_release.argocd, kubernetes_namespace.root_apps]
+}
